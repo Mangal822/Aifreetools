402f84b694c198149a10ec13533809461bcdaa1b
Mangal panwar <mangalpanwar678@gmail.com>
2026-01-29 18:29:34 +0530

    Add function to open Adsterra Smart Link

---
+++ script.js
@@ -0,0 +1,7 @@
+// Your Adsterra Smart Link
+const smartLink = "https://www.effectivegatecpm.com/xpxnbhq8j?key=c69c664f971cf4b1f3483eab0397b857";
+
+// Function to open link when a tool button is clicked
+function openSmartLink() {
+  window.open(smartLink, "_blank");
+}
